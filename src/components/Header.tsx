871c75af987bf5a5031b6477bb6d715a236e2bf4
Zico Montibeller <zico.montibeller@gmail.com>
2023-10-05 10:53:16 -0300

    Minor css adjstuments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ export function Header() {
   const anchors = ['home', 'projects', 'contact'];
 
   return (
-    <header className="flex sticky top-0 z-10 border-b bg-white p-2 justify-center md:justify-start">
-      <h1 className="py-2 px-5">
+    <header className="flex sticky top-0 z-10 border-b bg-white p-2 justify-center md:justify-start shadow-sm">
+      <h1 className="mb-0 py-2 px-5 text-3xl font-bold">
         <a href="#home">Portfolio</a>
       </h1>
       <div className="justify-between w-100 hidden md:flex">
         <ul className="flex items-center">
           { anchors.map(anchor => (
-            <li className="flex gap-4">
+            <li className="flex">
               <a
-                className="p-2 capitalize" 
+                className="p-2 mx-1 capitalize" 
                 href={`#${anchor}`}
               >{anchor}</a>
             </li>
