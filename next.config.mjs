dfc71d99a46a1084962d4a7cf68217c1a00527b3
Zico Montibeller <zico.montibeller@gmail.com>
2023-10-12 13:30:07 -0300

    Add songs api

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,27 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
