b537106d38db7d434a501e67db327479e2621bf3
Zico Montibeller <zico.montibeller@gmail.com>
2023-10-26 18:07:14 -0300

    Add click event tracking on section about

--- src/utils/GoogleAnalytics.tsx
+++ src/utils/GoogleAnalytics.tsx
@@ -1,6 +1,12 @@
 import Script from "next/script";
 import { env } from "~/env.mjs";
 
+export function trackGoogleClick(eventName: string) {
+  gtag('event', 'click', {
+    target: eventName
+  });
+}
+
 export function GoogleAnalytics() {
   const id = env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
